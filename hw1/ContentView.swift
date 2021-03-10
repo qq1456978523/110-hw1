e47d85ee70f6b1d75a5d1ec2d714e13c01c065e9
qaz1212 <49808673+qaz12312@users.noreply.github.com>
2021-03-10 10:48:14 +0800

    Initial Commit

---
+++ hw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hw1
+//
+//  Created by User03 on 2021/3/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hw1/hw1App.swift
@@ -0,0 +1,17 @@
+//
+//  hw1App.swift
+//  hw1
+//
+//  Created by User03 on 2021/3/10.
+//
+
+import SwiftUI
+
+@main
+struct hw1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
